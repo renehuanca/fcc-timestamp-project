ca229bb1b9699034abd2a695345c41d2b9cbb98f
renehuanca <renehuanca999@gmail.com>
2022-12-07 17:14:31 -0400

    feat: has a api/:date

--- index.js
+++ index.js
@@ -19,14 +19,35 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date', (req, res) => {
+	const date = req.params.date
+	if (/\d{5,}/.test(date)) {
+		const dateNumber = parseInt(date)
+		res.json({
+			unix: dateNumber,
+			utc: new Date(dateNumber).toUTCString()
+		})
+	} else {
+		if (date.toString() == "Invalid Date") {
+			res.json({error: "Invalid date"})
+		} else {
+			res.json({
+				unix: date.valueOf(), 
+				utc: new Date(date).toUTCString()
+			})
+		}
+	}
+	
+})
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000 || process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
